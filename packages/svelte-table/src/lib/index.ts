68f908b5c46f140f171cd12e8526786d27b1c282
George Daskalakis <george.dask97@gmail.com>
2024-11-09 11:30:20 +0200

    fix: export pagination

--- packages/svelte-table/src/lib/index.ts
+++ packages/svelte-table/src/lib/index.ts
@@ -1,6 +1,7 @@
 import { TableDef } from './table.svelte.js';
 import type { Cell } from './types/cell.js';
 import type { Column } from './types/column.js';
+import type { Pagination } from './types/pagination.js';
 import type { Row } from './types/row.js';
 import type { TableOptions, Table } from './types/table.js';
 
@@ -8,4 +9,4 @@ export function createTable<T>(data: T[], columns: Column<T>[], options: TableOp
 	return new TableDef(data, columns, options);
 }
 
-export type { Cell, Column, Row, Table, TableOptions };
+export type { Cell, Column, Row, Table, TableOptions, Pagination };
